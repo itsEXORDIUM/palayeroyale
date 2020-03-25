6f1d096d04ad90303e0c2efd8bd6137b1266bde7
Caroline Chambre <caroline.chambre@cameros.fr>
2020-03-25 17:03:01 +0100

    individual news page

---
+++ pages/edito/_id.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="edito" class="news_detail wrap">
+      <div class="content">
+        <h1 v-html="title"></h1>
+        <div class="content_type" v-html="content"></div>
+        <nuxt-link class="action" to="/news"><span>Back to the News</span></nuxt-link>
+      </div>
+      <img :src="image" alt=""/>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      id : this.$route.params.id,
+      news : [],
+      title : '',
+      content : '',
+      image: ''
+    }
+  },
+  mounted() {
+    // Get News Posts
+    axios.get('http://51.15.241.193/wp-json/wp/v2/posts?_embed')
+      .then(response => {
+        this.news = response.data;
+        this.news.forEach(element => {
+          if (parseInt(element.id) == this.id) {
+            this.title = element.title.rendered;
+            this.content = element.content.rendered;
+            this.image = element._embedded['wp:featuredmedia']['0'].source_url;
+          }
+        });
+      })
+  }
+}
+</script>
