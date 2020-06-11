1dd544ffb5a7e05eb12957a30649ef6746bfea51
Carolynn <caroxlynn@gmail.com>
2020-06-11 14:41:26 +0200

    sucuqgcuiqgiugiug

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
     extend (config, { isDev, isClient }) {
       if (!isDev) {
         // relative links, please.
-        config.output.publicPath = './../_nuxt/'
+        config.output.publicPath = 'palayeroyale/_nuxt/'
       }
       return config;
     }
